121891e4fd64e78579463d73b1c9839a2ba366a5
Steve Hu <stevehu@gmail.com>
2017-08-23 10:45:27 -0400

    upgrade httpserver to light-4j 1.4.0

--- httpserver/src/main/java/Example.java
+++ httpserver/src/main/java/Example.java
@@ -2,18 +2,14 @@ import com.networknt.server.HandlerProvider;
 import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Methods;
 
 
 public class Example implements HandlerProvider {
     private static final String MESSAGE = "Hello World!";
-
     public HttpHandler getHandler() {
-        return Handlers.path()
-        .addPrefixPath("/", new HttpHandler() {
-                public void handleRequest(HttpServerExchange exchange) {
-                    exchange.getResponseSender().send(MESSAGE);
-            	}
-            }
-        );
+        return Handlers.routing()
+            .add(Methods.POST, "/post", exchange ->  exchange.getResponseSender().send(MESSAGE))
+            .add(Methods.GET, "/get", exchange -> exchange.getResponseSender().send(MESSAGE));
     }
 }
